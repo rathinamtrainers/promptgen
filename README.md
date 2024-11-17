22bbf0d2841753dd98206ed45f19b3dbcb20eb97
rathinamtrainers <rajan@rathinamtrainers.com>
2024-11-17 20:11:34 +0530

    Initial Files.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Installation Instructions
+1. Install Torch package
+```pip install torch --index-url https://download.pytorch.org/whl/cu118  ```
+2. 
---
+++ main.py
@@ -0,0 +1,10 @@
+
+class RAGManager:
+    def __init__(self, base_directory):
+        self.base_directory = base_directory
+
+
+
+
+if __name__ == "__main__":
+    rag_manager = RAGManager(base_directory="c:\\ragmanager\\userdata")
